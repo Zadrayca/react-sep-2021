cf87efe3fe850c540ec60f8b51f5cc362c0aa4d0
Zadrayca <coxatiy@gmail.com>
2022-01-13 15:20:47 +0200

    Classwork 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Rokets from "./components/Rokets";
 
 function App() {
   return (
     <div className="App">
-
+      <div>
+          <Rokets/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Rokets.js
@@ -0,0 +1,40 @@
+import {useEffect, useState} from "react";
+
+
+export default function Rokets(props) {
+
+    let [rokets, setRokets] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(value => {
+                setRokets(value);
+            })
+
+    }, []);
+    let result = rokets.filter(roket => roket.launch_year !== "2020");
+
+    let count = 0;
+
+    return (
+        <div>
+            {
+                result.map(value =>
+                <div className={'bigBox'} key={count+=1}>
+                    <div className={'miniBox'}>
+                        <div>{value.mission_name}</div>
+                        <div>{value.launch_year}</div>
+                    </div>
+                    <div className={'miniBox'}>
+                        <img src={value.links.mission_patch_small} alt="mission_patch_small"/>
+                    </div>
+
+                </div>
+                )
+            }
+        </div>
+    )
+
+
+}
