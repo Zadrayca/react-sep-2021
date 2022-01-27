c8b8923c034327b258811c6a2c1cac512aed3b3b
Zadrayca <coxatiy@gmail.com>
2022-01-27 19:27:18 +0200

    Classwork 2

--- src/components/Form/Forms.js
+++ src/components/Form/Forms.js
@@ -7,15 +7,21 @@ const Form = ({getFilter}) => {
     const formHandler = (e) => {
         const eventData = {...form, [e.target.name]: e.target.value};
         setForm({...form, ...eventData})
-        getFilter(eventData)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        getFilter(form)
     }
 
     return (
         <div>
-            <form action="">
+            <form>
                 <label>Name: <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label>
-                <label> Username: <input type="text" name={'username'} value={form.username} onChange={formHandler}/></label>
+                <label> Username: <input type="text" name={'username'} value={form.username}
+                                         onChange={formHandler}/></label>
                 <label> Email: <input type="text" name={'email'} value={form.email} onChange={formHandler}/></label>
+                <button onClick={onSubmit}>Filter</button>
             </form>
         </div>
     );
