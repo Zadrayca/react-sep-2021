e4bb66a4f5ca9c409ab5767b7b779b3bd360e9f4
Zadrayca <coxatiy@gmail.com>
2022-02-11 00:50:38 +0200

    without redux

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import {useReducer} from "react";
-
 import css from "./App.module.css"
-import {Forms} from "./Components/Forms/Forms";
-import {Cats} from "./Components/Cats/Cats";
-import {Dogs} from "./Components/Dogs/Dogs";
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_CAT':
-            return {...state, cats: [...state.cats, {id:new Date().getTime(), name: action.payload.cat}]};
-        case 'ADD_DOG':
-            return {...state, dogs: [...state.dogs, {id:new Date().getTime(), name: action.payload.dog}]};
-        case 'DEL_CAT':
-            return {...state, cats: state.cats.filter(cat => cat.id !== action.payload.id)};
-        case 'DEL_DOG':
-            return {...state, dogs: state.dogs.filter(dog => dog.id !== action.payload.id)};
-        default:
-            return state;
-    }
-}
+import {useState} from "react";
+import {Form} from "./Components/Form/Form";
+import {Cars} from "./Components/Cars/Cars";
 
 function App() {
 
-    const [{cats, dogs}, dispatch] = useReducer(reducer, {cats: [], dogs: []});
+    const [cars, setCars] = useState([]);
+
+    const getFormData = (data) => {
+        setCars([...cars, {id: new Date().getTime(), ...data}]);
+    }
+
+    const getCarId = (id) => {
+        setCars(cars.filter(car => car.id !== id))
+    }
 
     return (
         <div className={css.bigBox}>
-            <div><Forms dispatch={dispatch}/></div>
-            <div className={css.catDog}>
-                <Cats cats={cats} dispatch={dispatch}/>
-                <Dogs dogs={dogs} dispatch={dispatch}/>
-            </div>
+            <div><Form getFormData={getFormData}/></div>
+            <div><Cars cars={cars} getCarId={getCarId}/></div>
         </div>
     );
 }
